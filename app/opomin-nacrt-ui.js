3758b0863887dc8a1e59af66feb4bf8e3b7f4246
Uspesni Jezek <uspesenjezek@gmail.com>
2026-08-11 11:52:29 +0200

    Popravek crasha: odstranjeni ostanki stare kode pri gumbih

--- app/opomin-nacrt-ui.js
+++ app/opomin-nacrt-ui.js
@@ -2367,9 +2367,6 @@
                     : "") +
                   '" id="opomin-zdaj-cas" aria-label="Nastavi trenutno uro">Trenutna ura</button>'
                 : "") +
-                ? " opomin-nacrt__gumb-zdaj--aktiven"
-                : "") +
-              '" id="opomin-zdaj-cas" aria-label="Nastavi trenutno uro">Trenutna ura</button>' +
               '<span class="opomin-nacrt__predizbor-ovoj">' +
               '<button type="button" class="opomin-nacrt__gumb-predizbor' +
               (izbranCasNacin === "predizbor"
